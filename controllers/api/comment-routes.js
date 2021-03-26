6bd99beb1cb201dd936373eb3606acf99eae48df
DragoonKite <frank.depaolo@yahoo.com>
2021-03-26 16:39:14 -0500

    added and tested comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,51 @@
 const router = require('express').Router();
-const {User, Post, Comment} = require('../../models');
+const {Comment} = require('../../models');
+
+//GET all comments
+router.get('/', (req, res) => {
+    Comment.findAll({
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//POST create comment
+router.post('/', (req, res) => {
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+        });
+    }
+});
+
+//DELETE comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+          id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+        res.status(404).json({ message: 'No comment found with this id' });
+        return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
