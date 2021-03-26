81983d45da8c7c28055a26a76aa325737e66ea29
DragoonKite <frank.depaolo@yahoo.com>
2021-03-25 23:36:04 -0500

    set up skeleton for user-routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {User, Post} = require('../../models');
+
+//GET /api/users
+
+//GET /api/users/:id
+
+//POST /api/users
+
+//POST /login
+
+//POST /logout
+
+//PUT /api/users/:id
+
+//DELETE /api/users/:id
+
+module.exports = router;
